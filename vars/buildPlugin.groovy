311d9980a39c4b48ad976a0c5ef31450decfce7d
farooqiafridi <61327719+farooqiafridi@users.noreply.github.com>
2021-05-15 01:32:29 -0500

    Update buildPlugin.groovy

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -1,7 +1,7 @@
 def call(Map config) {
     node {
         git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
-        sh 'mvn install'
+        bat 'mvn install'
         mail to: '...', subject: "${config.name} plugin build", body: '...'
     }
 }
