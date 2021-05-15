500a2b9a52c7e75c2ee2d9336d7c876bf0b32928
farooqiafridi <61327719+farooqiafridi@users.noreply.github.com>
2021-05-15 02:09:20 -0500

    Update evenOrOdd.groovy

--- vars/evenOrOdd.groovy
+++ vars/evenOrOdd.groovy
@@ -10,16 +10,4 @@ def call(int buildNumber) {
         }
       }
     }
-  } else {
-    pipeline {
-      agent any
-      stages {
-        stage('Odd Stage') {
-          steps {
-            echo "The build number is odd"
-          }
-        }
-      }
-    }
   }
-}
